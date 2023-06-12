a6c291c4c2e8529b50ec865d6e4f0806705d0a43
TLBail <theolebail02@gmail.com>
2023-06-12 09:45:32 +0200

    fix fragments layout typo

--- app/src/test/java/com/example/super_cep/CreateSlideShowTest.java
+++ app/src/test/java/com/example/super_cep/CreateSlideShowTest.java
@@ -5,12 +5,16 @@ import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.apache.poi.xslf.usermodel.XSLFSlideLayout;
 import org.apache.poi.xslf.usermodel.XSLFSlideMaster;
+import org.apache.poi.xslf.usermodel.XSLFTable;
+import org.apache.poi.xslf.usermodel.XSLFTableCell;
+import org.apache.poi.xslf.usermodel.XSLFTableRow;
 import org.apache.poi.xslf.usermodel.XSLFTextBox;
 import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.awt.Color;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -74,5 +78,94 @@ public class CreateSlideShowTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    public void createSlideWithTable() {
+        try {
+            XMLSlideShow ppt = new XMLSlideShow();
+
+            // Créer une diapositive
+            XSLFSlide slide = ppt.createSlide();
+
+            // Créer un tableau
+            XSLFTable tbl = slide.createTable();
+            tbl.setAnchor(new java.awt.Rectangle(50, 50, 450, 300));
+
+            // Créer une ligne de titre
+            XSLFTableRow headerRow = tbl.addRow();
+            headerRow.addCell().setText("Nom");
+            headerRow.addCell().setText("Age");
+
+            // Ajouter des données d'exemples
+            XSLFTableRow row1 = tbl.addRow();
+            row1.addCell().setText("Alice");
+            row1.addCell().setText("25");
+
+            XSLFTableRow row2 = tbl.addRow();
+            row2.addCell().setText("Bob");
+            row2.addCell().setText("30");
+
+            // Enregistrer le résultat
+            try (FileOutputStream out = new FileOutputStream("test.pptx")) {
+                ppt.write(out);
+            }
+            Assert.assertTrue(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.assertTrue(true);
+
+        }
+    }
+
+    @Test
+    public void createSlideWithColoredTable() {
+        try {
+            XMLSlideShow ppt = new XMLSlideShow();
+
+            // Créer une diapositive
+            XSLFSlide slide = ppt.createSlide();
+
+            // Créer un tableau
+            XSLFTable tbl = slide.createTable();
+            tbl.setAnchor(new java.awt.Rectangle(50, 50, 450, 300));
+
+            // Créer une ligne de titre
+            XSLFTableRow headerRow = tbl.addRow();
+            headerRow.addCell().setText("Nom");
+            headerRow.addCell().setText("Age");
+
+            // Ajouter des données d'exemples
+            XSLFTableRow row1 = tbl.addRow();
+            XSLFTableCell cell1 = row1.addCell();
+            cell1.setText("Alice");
+            cell1.setFillColor(Color.RED);
+
+            XSLFTableCell cell2 = row1.addCell();
+            cell2.setText("25");
+            cell2.setFillColor(Color.BLUE);
+
+            XSLFTableRow row2 = tbl.addRow();
+            XSLFTableCell cell3 = row2.addCell();
+            cell3.setText("Bob");
+            cell3.setFillColor(Color.RED);
+
+            XSLFTableCell cell4 = row2.addCell();
+            cell4.setText("30");
+            cell4.setFillColor(Color.BLUE);
+
+            // Enregistrer le résultat
+            try (FileOutputStream out = new FileOutputStream("test.pptx")) {
+                ppt.write(out);
+            }
+
+            Assert.assertTrue(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.assertTrue(true);
+
+        }
+    }
+
 
 }
