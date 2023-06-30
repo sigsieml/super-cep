229276f9dffdbc756c951245e1b27f58f56ddde2
TLBail <theolebail02@gmail.com>
2023-06-30 16:31:12 +0200

    Changement des police par défault pour les cellules vide  #29

--- app/src/test/java/com/example/super_cep/model/SpinnerData/JsonConfigDataManagerTest.java
+++ app/src/test/java/com/example/super_cep/model/SpinnerData/JsonConfigDataManagerTest.java
@@ -7,12 +7,14 @@ import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class JsonConfigDataManagerTest {
 
     @Test
     public void testSerialize() {
-        String dataProvider = JsonConfigDataManager.serialize(new ConfigDataProvider());
+        String dataProvider = JsonConfigDataManager.serialize(new ConfigDataProvider(Map.of("key", List.of("value"))));
         try (FileOutputStream out = new FileOutputStream("configData.json")) {
             out.write(dataProvider.getBytes());
         } catch (FileNotFoundException e) {
